ff6a5734921d841a93c06101970f40ea68bf7d09
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-06-14 09:18:12 -0300

    feat: Add revalidate api to revalidate SSG when a new art is created

--- frontend/src/pages/api/revalidate.ts
+++ frontend/src/pages/api/revalidate.ts
@@ -2,7 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-
     if (req.query.secret !== process.env.NEXT_PUBLIC_REVALIDATE_SECRET) {
         return res.status(401).json({ message: 'Invalid Token' })
     }

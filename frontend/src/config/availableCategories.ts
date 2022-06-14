89b6fd5512d42a46b5c7b4c069288628897b3a92
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-06-14 09:17:07 -0300

    update: update ArtSchema and deleted availableCategories config

--- frontend/src/config/availableCategories.ts
+++
@@ -1,6 +0,0 @@
-export const availableCategories = [
-    'drawing',
-    'painting',
-    'craftsmanship',
-    'string_art'
-]
--- frontend/src/schemas/Art.ts
+++ frontend/src/schemas/Art.ts
@@ -4,7 +4,7 @@ import { availableImageTypes } from '../config/availableImageType'
 export interface ArtSchema {
     id: string
     title: string
-    category: "painting" | "drawing" | "craftsmanship" | "string_art"
+    categoryTitle: string
     description?: string
     image: string
     dimension: string
@@ -15,7 +15,7 @@ export interface ArtSchema {
 export const saveArtYupSchema = yup.object().shape({
     title: yup.string().required('Title is required'),
     description: yup.string(),
-    category: yup.string().required('Category is required'),
+    categoryTitle: yup.string().required('Category is required'),
     file: yup.mixed().required('Image is Required').test('fileFormat', 'Image Only', value => {
         if (value) {
             return availableImageTypes.includes(value.type)
@@ -31,7 +31,7 @@ export const saveArtYupSchema = yup.object().shape({
 export const updateArtYupSchema = yup.object().shape({
     title: yup.string().required('Title is required'),
     description: yup.string(),
-    category: yup.string().required('Category is required'),
+    categoryTitle: yup.string().required('Category is required'),
     dimension: yup.string().required('Dimension is required'),
     uniqueCode: yup.string().required('Unique Code is Required'),
     productionDate: yup.date().required('Production Date is Required'),

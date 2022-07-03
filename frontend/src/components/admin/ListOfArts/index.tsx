885e85ede7196e0c76958e1396f51b7afde57edd
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-07-03 18:20:48 -0300

    feat: Add table to show arts on admin page

--- frontend/src/components/admin/ListOfArts/index.tsx
+++ frontend/src/components/admin/ListOfArts/index.tsx
@@ -1,22 +1,58 @@
 import { useState } from "react";
-import { SearchBar } from "../../SearchBar";
-import { ArtOrCategory } from "../ArtOrCategory";
+import DeleteIcon from '@mui/icons-material/Delete';
+import InfoIcon from '@mui/icons-material/Info';
+import EditIcon from '@mui/icons-material/Edit';
+import { SearchBar } from "../SearchBar";
 import { Container } from "./style";
+import { useArts } from "../../../hooks/useArts";
 
 export function ListOfArts(): JSX.Element {
 
     const [searchingFor, setSearchingFor] = useState('title')
     const [currentInput, setCurrentInput] = useState('')
 
+    const { arts } = useArts()
+
     return (
         <Container>
-            <ArtOrCategory />
             <SearchBar
                 searchingFor={searchingFor}
                 setSearchingFor={setSearchingFor}
                 currentInput={currentInput}
                 setCurrentInput={setCurrentInput}
             />
+
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                        <th>Image</th>
+                        <th>Title</th>
+                        <th>Category</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {arts?.map(art => (
+                        <tr key={art.id}>
+                            <td><EditIcon /></td>
+                            <td><DeleteIcon /></td>
+                            <td>
+                                <div className="imageContainer">
+                                    <img src={art.image} alt={art.title} />
+                                </div>
+                            </td>
+                            <td>
+                                <span className="title">
+                                    <InfoIcon />
+                                    {art.title}
+                                </span>
+                            </td>
+                            <td>{art.categoryTitle}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </Container>
     )
 }
--- frontend/src/components/admin/ListOfArts/style.ts
+++ frontend/src/components/admin/ListOfArts/style.ts
@@ -2,5 +2,57 @@ import styled from "styled-components";
 
 export const Container = styled.section`
     
+    table {
+        width: 100%;
+
+        thead tr {
+            margin-bottom: 0.5rem;
+        }
+
+        tbody tr {
+            padding: 0 0.5rem;
+        }
+
+        tbody tr, thead tr{
+            width: 100%;
+
+            display: grid;
+            grid-template-columns: 0.5fr 0.5fr 2fr 4fr 2fr;
+
+            td {
+                border-bottom: 1px dashed ${props => props.theme.colors.textOne};
+            }
+
+            th, td {
+                display: flex;
+                align-items: center;
+                text-align: left;
+                color: ${props => props.theme.colors.textOne};
+
+                .title {
+                    display: flex;
+                    align-items: center;
+                    justify-content: flex-start;
+                    gap: 0.5rem;
+
+                    svg {
+                        font-size: 1rem;
+                    }
+                }
+
+                .imageContainer {
+                    max-width: 40px;
+                    padding: 0.125rem;
+                    display: flex;
+                    align-items: center;
+
+                    img {
+                        object-fit: contain;
+                        width: 100%;
+                    }
+                }
+            }
+        }
+    }
 
 `

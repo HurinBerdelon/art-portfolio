23462de67250e3033d016baa387cd1fa86117f6d
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-10-17 22:07:36 -0300

    fix: fix bug in scroll on admin page

--- frontend/src/components/admin/ListOfArts/style.ts
+++ frontend/src/components/admin/ListOfArts/style.ts
@@ -4,7 +4,6 @@ export const Container = styled.section`
     
     display: flex;
     flex-direction: column;
-    overflow: auto;
 
     .updateArtsPages {
         align-self: center;
--- frontend/src/components/admin/ListOfCategories/style.ts
+++ frontend/src/components/admin/ListOfCategories/style.ts
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Container = styled.section`
 
     margin-top: 1rem;
-    overflow: auto;
 
     table {
         width: 100%;
